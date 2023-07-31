12fe34d6f74de295c333f703a9f9cc0face48884
Jennyads <jennycristina.1998@gmail.com>
2023-07-31 20:58:43 -0300

    send dishes to api

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,15 +1,17 @@
+import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { IPaginacao } from '../../interfaces/IPaginacao';
 import IRestaurante from '../../interfaces/IRestaurante';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
-import axios from 'axios';
-import { IPaginacao } from '../../interfaces/IPaginacao';
 
 const ListaRestaurantes = () => {
+
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
   const [proximaPagina, setProximaPagina] = useState('')
 
   useEffect(() => {
+    // obter restaurantes
     axios.get<IPaginacao<IRestaurante>>('http://localhost:8000/api/v1/restaurantes/')
       .then(resposta => {
         setRestaurantes(resposta.data.results)
@@ -30,11 +32,12 @@ const ListaRestaurantes = () => {
         console.log(erro)
       })
   }
+
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
     {proximaPagina && <button onClick={verMais}>
-      Ver mais
+      ver mais
     </button>}
   </section>)
 }
